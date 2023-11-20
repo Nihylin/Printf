3851714c16e1533c0f0bcd88ead0d7c5d440f31b
Nihylin <nihylin@gmail.com>
2023-11-20 15:29:17 +0100

    Add base files (main.h, _printf.c, _putchar.c ...)

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _printf - prints formatted strings
+ * @format: specified format
+ * Return: int (number of characters printed)
+*/
+
+int _printf(const char *format, ...)
+{
+
+}
---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Bibliothèques
+*/
+
+#include <limits.h>
+#include <stdio.h>
+
+/**
+ * Prototypes
+*/
+
+int _printf(const char *format, ...);
+
+#endif
