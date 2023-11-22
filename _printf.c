022dbe7647be4abc91ade0965abb8729ed6fa611
Diesos <ozturk@live.Fr>
2023-11-22 16:27:32 +0100

    added condition following the flowchart

--- _printf.c
+++ _printf.c
@@ -4,19 +4,45 @@
  * _printf - prints formatted strings
  * @format: specified format
  * Return: int (number of characters printed)
-*/
+ */
 
-int _printf(const char *format, ...)
+int	_printf(const char *format, ...)
 {
-	int count = 0;
-	int (*function)(va_list);
-	va_list args;
-	
-	va_start (args, format);
+	int		count;
+	int		(*function)(va_list);
+	va_list	args;
+	char	*next_Character;
 
-	/* Stuff happens */
-
-	va_end (args);
-
+	count = 0;
+	va_start(args, format);
+	next_Character = (args + 1);
+	if (format == NULL)
+		return (-1);
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			_putchar(*format);
+			count++;
+			format++;
+		}
+		if (*format == '%')
+		{
+			format++;
+			function = _getFunction(next_Character);
+			if (function)
+			{
+				count += function(args);
+			}
+			else
+			{
+				_putchar(*format);
+				count += 2;
+			}
+		}
+		format++;
+		count++;
+	}
+	va_end(args);
 	return (count);
 }
